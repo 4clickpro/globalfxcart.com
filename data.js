e5374fd52815a8cf81788d8c0681cca31804b206
Paul Webb <4clickpro@gmail.com>
2026-09-06 09:00:01 -0500

    Auto-publish: Rental Review: Our $99/Day Golf Cart in Miramar Beach Was the Highlight

--- data.js
+++ data.js
@@ -351,6 +351,14 @@ const reviews = [
     image: "images/cart-4seat.webp",
     link: "99-rental-review-silver-sands-outlets-99.html",
     date: "September 5, 2026"
+  },
+  {
+    id: 52,
+    title: "Rental Review: Our $99/Day Golf Cart in Miramar Beach Was the Highlight",
+    excerpt: "“We rented a 4-seat cart for the week and it made the whole trip.” A real customer’s experience with the $99/day Miramar Beach golf cart rental.",
+    image: "images/cart-4seat.webp",
+    link: "99-rental-review-family-miramar-beach-103.html",
+    date: "September 6, 2026"
   }];
 
 const blogPosts = [
